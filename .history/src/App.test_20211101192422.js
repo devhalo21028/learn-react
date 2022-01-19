9497cf30a3dc69a23502b8c3dabd281e9fd9ebdb
Jayson Ojeda <jayson.unto.ojeda@gmail.com>
2022-01-19 15:42:09 +0800

    remove .history

--- .history/src/App.test_20211101192422.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  expect(screen.getByText(/Test App/i)).toBeInTheDocument();
-  expect(screen.getByText(/Sign In/i)).toBeInTheDocument()
-});
--- .history/src/App.test_20211101192436.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-it('renders initial page', () => {
-  render(<App />);
-  expect(screen.getByText(/Test App/i)).toBeInTheDocument();
-  expect(screen.getByText(/Sign In/i)).toBeInTheDocument()
-});
